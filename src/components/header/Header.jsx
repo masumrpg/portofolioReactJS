acfaef1bfce4acbe5152e5cd6983fa0e5802e3ec
masumrpg <masumrpg@gmail.com>
2023-05-08 10:02:20 +0700

    Sampe header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Halo Nama Saya</h5>
+        <h1>Ma'sum</h1>
+        <h5 className="text-light">FrontEnd Developer</h5>
+        <CTA/>
+      </div>
+    </header>
   )
 }
 
