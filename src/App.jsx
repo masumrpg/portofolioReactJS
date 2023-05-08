35d7e23899a6abfdd73d4f07f8e8649d4653fd0b
masumrpg <masumrpg@gmail.com>
2023-05-08 09:03:35 +0700

    Masih first

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portofolio from './components/portofolio/Portofolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portofolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </>
   )
 }
 
