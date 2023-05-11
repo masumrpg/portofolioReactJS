2f57f2e04fedcaa43e10815fc31cee47191bcdbc
Masum <masumrpg@gmail.com>
2023-05-11 07:31:57 +0700

    Button Primarynya Kurang di tengahin lainya udah

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className="btn">Unduh CV</a>
-        <a href="#contact" className="btn btn-primary">Kontak Saya</a>
+        <a href={CV} download className="btn">Download CV</a>
+        <a href="#contact" className="btn btn-primary">Contact Me</a>
     </div>
   )
 }
