3102c5e6f9cac6ea0564ab3b9bc7eacd6cc171e4
masumrpg <masumrpg@gmail.com>
2023-05-12 22:53:54 +0700

    Contact Kelar

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import emailjs from '@emailjs/browser'
+import EmailSent from './EmailSent'
+// import { useState } from 'react';
 import './contact.css'
+import {HiOutlineMail} from 'react-icons/hi'
+import {FaFacebookMessenger} from 'react-icons/fa'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
+  const form = useRef()
+  //Percobaan
+  const [emailSent, setEmailSent] = useState(false)
+
+  useEffect(() => {
+    let timer;
+    if (emailSent) {
+      timer = setTimeout(() => {
+        setEmailSent(false)
+      }, 5000)
+    }
+    return () => clearTimeout(timer);
+  }, [emailSent])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_1czp9ns', 'template_ebjr4yb', form.current, '44RxuASXDJFAdKPDK')
+
+    e.target.reset()
+    setEmailSent(true)
+  }
+
   return (
     <section id="contact">
-      
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact-container">
+        <div className="contact-options">
+          <article className="contact-option">
+            <HiOutlineMail className='contact-option-icon'/>
+            <h4>Email</h4>
+            <h5>masumrpg@gmail.com</h5>
+            <a href="mailto:masumrpg@gmail.com">Send a Message</a>
+          </article>
+          <article className="contact-option">
+            <FaFacebookMessenger className='contact-option-icon'/>
+            <h4>Messenger</h4>
+            <h5>masum.rpg</h5>
+            <a href="http://m.me/masum.rpg" target="_blank" rel="noreferrer">Send a Message</a>
+          </article>
+          <article className="contact-option">
+            <BsWhatsapp className='contact-option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>+6287729647721</h5>
+            <a href="http://wa.me/6287729647721" target="_blank" rel="noreferrer">Send a Message</a>
+          </article>
+        </div>
+        
+
+        {/* ---------- FORM ---------- */}
+
+        {emailSent ? <EmailSent /> : (
+          <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+        )}
+        
+      </div>
     </section>
   )
 }
