fb08851f428574ecb3eb55f1fe31e996c15f8f08
masumrpg <masumrpg@gmail.com>
2023-07-12 20:30:13 +0700

    Disable warning

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,34 +1,55 @@
-import React from 'react'
-import './footer.css'
-import { FaFacebook } from 'react-icons/fa'
-import { AiFillInstagram } from 'react-icons/ai'
-import { AiFillLinkedin } from 'react-icons/ai'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./footer.css";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer-logo">MA'SUM</a>
+      <a href="#" className="footer-logo">
+        MA'SUM
+      </a>
 
       <ul className="permalinks">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#education">Education</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#portofolio">Portofolio</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#education">Education</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portofolio">Portofolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
       <div className="footer-socials">
-        <a href="https://facebook.com/masum.rpg"><FaFacebook/></a>
-        <a href="https://instagram.com/masum.rpg"><AiFillInstagram/></a>
-        <a href="https://www.linkedin.com/in/ma-sum-575068196/"><AiFillLinkedin/></a>
+        <a href="https://facebook.com/masum.rpg">
+          <FaFacebook />
+        </a>
+        <a href="https://instagram.com/masum.rpg">
+          <AiFillInstagram />
+        </a>
+        <a href="https://www.linkedin.com/in/ma-sum-575068196/">
+          <AiFillLinkedin />
+        </a>
       </div>
 
       <div className="footer-copyright">
         <small>&copy; 2023. Allright reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
